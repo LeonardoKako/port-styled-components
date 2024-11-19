63c1febbe91c80993763905e0367f4129e407766
LeonardoKako <fleithkako@gmail.com>
2024-11-19 18:43:51 -0300

    projetos

---
+++ src/components/Projeto/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+  padding: 16px;
+  border: solid 1px #c1c1c1;
+`
+
+export const LinkBotao = styled.a`
+  color: #fff;
+  font-size: 14px;
+  background-color: #44768f;
+  text-decoration: none;
+  padding: 8px;
+  display: inline-block;
+  margin-top: 24px;
+`
--- src/style.ts
+++ src/style.ts
@@ -3,9 +3,10 @@ import styled, { createGlobalStyle } from 'styled-components'
 const EstiloGlobal = createGlobalStyle`
   * {
     margin: 0;
-    padding: 0
+    padding: 0;
     font-family: 'Inter', sans-serif;
-      }
+    list-style: none;
+    }
 
   body {
     padding-top: 80px;
