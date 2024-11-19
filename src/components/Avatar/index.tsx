a16727803fcc815eaa82ae04dfe4530ea958a7a8
LeonardoKako <fleithkako@gmail.com>
2024-11-19 19:03:26 -0300

    Estilizando a Sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+export const Avatar = () => {
+  return <FotoPerfil src="https://github.com/leonardokako.png" />
+}
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,11 +1,23 @@
+import { Avatar } from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
 
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 const Sidebar = () => {
   // eslint-disable-next-line prettier/prettier
   return (
     <aside>
-      <img src="https://github.com/leonardokako.png" alt="" />
-      <Titulo fontSize={20}>Leonardo Fleith</Titulo>
+      <SidebarContainer>
+        <Avatar />
+        <Titulo fontSize={18}>Leonardo Fleith</Titulo>
+        <Paragrafo tipo="secundario" fontSize={16}>
+          leonardokako
+        </Paragrafo>
+        <Descricao tipo="principal" fontSize={12}>
+          Engenheiro Front-end
+        </Descricao>
+        <BotaoTema>Trocar tema</BotaoTema>
+      </SidebarContainer>
     </aside>
   )
 }
